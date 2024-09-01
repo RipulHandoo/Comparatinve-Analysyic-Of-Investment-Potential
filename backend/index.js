4da51d560484332687ab49166fa3e0df519c4588
Ripul Handoo <ripulhandoo1234@gmail.com>
2024-09-01 23:08:53 +0530

    C:/Program Files (x86)/Git/market page route set in backend service
    Signed-off-by: Ripul Handoo <ripulhandoo1234@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const router = require("./routes/test")
 const searchRouter = require("./routes/searchResult")
 const userRouter = require("./routes/user")
+const marketRouter = require("./routes/market")
 const bodyParser = require("body-parser");
 
 app.use(cors())
@@ -12,6 +13,7 @@ app.use(bodyParser.json())
 app.use(router)
 app.use(userRouter)
 app.use(searchRouter)
+app.use(marketRouter)
 
 app.listen(3000, () => {
     console.log(`Server is running on PORT ${3000}`);
