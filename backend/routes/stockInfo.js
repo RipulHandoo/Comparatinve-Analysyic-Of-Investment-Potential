ed2a2724a4bab9a6d04b4f2310758a3e8bee2dfd
Ripul Handoo <ripulhandoo1234@gmail.com>
2024-09-04 23:48:23 +0530

    Backend service for fetching the upper and lower limit of the stock
    Signed-off-by: Ripul Handoo <ripulhandoo1234@gmail.com>

--- backend/routes/stockInfo.js
+++ backend/routes/stockInfo.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const GetFundamentals = require('../services/fundamentals');
+const UpperLowerLimit = require('../services/upperLowerLimit');
 
 const infoRouter = express.Router()
 
 infoRouter.get("/api/v1/stock/info/fundamentals", GetFundamentals)
+infoRouter.get("/api/v1/stock/info/upperLowerLimit", UpperLowerLimit)
 
 module.exports = infoRouter;
