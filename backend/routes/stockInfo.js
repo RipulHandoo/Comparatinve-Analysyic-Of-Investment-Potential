7801fa9ae60faa1e1d2a74544865352e8ff2d3f3
Ripul Handoo <ripulhandoo1234@gmail.com>
2024-09-07 19:53:39 +0530

    Routes set for stock related information
    Signed-off-by: Ripul Handoo <ripulhandoo1234@gmail.com>

--- backend/routes/stockInfo.js
+++ backend/routes/stockInfo.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const GetFundamentals = require('../services/fundamentals');
 const UpperLowerLimit = require('../services/upperLowerLimit');
+const CurrRate = require('../services/currRate');
+const StockData = require('../services/stockData');
 
 const infoRouter = express.Router()
 
 infoRouter.get("/api/v1/stock/info/fundamentals", GetFundamentals)
 infoRouter.get("/api/v1/stock/info/upperLowerLimit", UpperLowerLimit)
-
+infoRouter.get("/api/v1/stock/info/currRate", CurrRate)
+infoRouter.get("/api/v1/stock/info/data", StockData)
 module.exports = infoRouter;
